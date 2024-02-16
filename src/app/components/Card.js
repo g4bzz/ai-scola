ac5fd695f911103c8b9f5a109f1bc25efb5b23f2
Gabriel Oliveira <66384277+G4bzz@users.noreply.github.com>
2024-02-15 23:22:28 -0300

    feat: init proj

---
+++ src/app/components/Card.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Card = ({ image, title, description }) => {
+  return (
+    <div className="max-w-xs rounded overflow-hidden shadow-lg transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105">
+      <div
+        className="bg-cover bg-center h-64"
+        style={{ backgroundImage: `url(${image})` }}
+      >
+      </div>
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">{title}</div>
+        <p className="text-gray-700 text-base">{description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
