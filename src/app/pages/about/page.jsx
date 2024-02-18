d9c9f8727c73b5883ee7c0dd5ba6c8bf9d5d35b1
Gabriel Oliveira <66384277+G4bzz@users.noreply.github.com>
2024-02-18 19:15:23 -0300

    feat: adiciona texto da página "sobre nós"

--- src/app/pages/about/page.jsx
+++ src/app/pages/about/page.jsx
@@ -1,17 +1,32 @@
 "use client"; /* se nao usar essa zorra a pagina nao renderiza: https://github.com/tailwindlabs/headlessui/issues/1980 */
 
 export default function About() {
-
+	const nomes = [
+    'Gabriel de Oliveira Santos',
+    'Gustavo Caetano Santos',
+    'Vitor Hugo Ribeiro Tiburtino de Melo'
+  ];
   return (
 		<>
-			<header className="bg-white shadow">
+			<header className="bg-white shadow dark:bg-zinc-700">
 				<div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-					<h1 className="text-3xl font-bold tracking-tight text-gray-900">Sobre nós</h1>
+					<h1 className="text-3xl font-bold tracking-tight text-gray-900 dark:text-gray-200">Sobre Nós</h1>
 				</div>
 			</header>
 			<main>
-				<div className="mx-auto justify-center max-w-7xl py-6 sm:px-6 lg:px-8 grid lg:grid-cols-3 gap-x-3 gap-y-6 md:grid-cols-2 sm:grid-cols-1">
-					<p>bvbbbbbbbbb</p>
+				<div className="mx-auto w-3/5 h-svh text-lg dark:text-gray-100 justify-center max-w-7xl py-6 sm:px-6 lg:px-8">
+					<p>Somos uma equipe apaixonada por simplificar o conhecimento para jovens. Nosso site tem como objetivo transaformar textos e histórias complexas eme algo mais acessível e envolvente, especialmente para os mais jovens. Utilizamos tecnologia de Inteligência Artificial (IA) para narrar esses conteúdos por meio de áudios cativantes.</p>
+					<p>Conheça os integrantes da nossa equipe:</p>
+					<ul className="list-disc ml-5">
+						{nomes.map((nome, index) => (
+							<li key={index} className="py-1 ml-2">
+								{nome}
+							</li>
+						))}
+					</ul>
+					<p>
+						Jutos, estamos comprometidos em simplificar o mundo das palavras e inspirar a próxima geração de leitores e pensadores. Seja bem-vindo ao nosso site, onde a IA dá vida às histórias e torna o aprendizado uma experiência única e acessível.
+					</p>
 				</div>
 			</main>
 		</>
